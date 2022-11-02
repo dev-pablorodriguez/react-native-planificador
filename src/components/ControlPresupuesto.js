4e4315c65aaa8ef5c689612133e0357d8562364a
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-02 18:40:46 -0300

    Progress Indicator implementado.

--- src/components/ControlPresupuesto.js
+++ src/components/ControlPresupuesto.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { Text, View, Image, StyleSheet } from 'react-native'
 import globalStyles from '../styles'
 import { formatearCantidadDolar }  from '../helpers'
+import CircularProgress from 'react-native-circular-progress-indicator' 
 
 const ControlPresupuesto = ({ presupuesto, gastos }) => {
     const [disponible, setDisponible] = useState(0);
     const [gastado, setGastado] = useState(0);
+    const [porcentaje, setPorcentaje] = useState(0);
 
     useEffect( () => {
         const totalGastado = gastos.reduce( ( total, item ) => {
@@ -14,14 +16,30 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
 
         const totalDisponible = presupuesto - totalGastado;
 
+        const nuevoPorcentaje = totalGastado*100/presupuesto;
+
         setDisponible(totalDisponible);
         setGastado(totalGastado);
+        setTimeout( () => setPorcentaje(nuevoPorcentaje), 1000 )
     }, [ gastos ])
 
     return (
         <View style={ styles.contenedor }>
             <View style={ styles.centrarGrafica }>
-                <Image style={ styles.imagen } source={ require('../img/grafico.jpg') }/>
+                {/* <Image style={ styles.imagen } source={ require('../img/grafico.jpg') }/> */}
+                <CircularProgress 
+                    value={ porcentaje } 
+                    duration={ 500 }
+                    radius={ 150 }
+                    valueSuffix={ '%' }
+                    title='Gastado'
+                    inActiveStrokeColor='#F5F5F5'
+                    inActiveStrokeWidth={ 20 }
+                    activeStrokeColor='#3B82F6'
+                    activeStrokeWidth={ 20 }
+                    titleStyle={{ fontWeight: 'bold', fontSize: 20 }}
+                    titleColor='#64748B'
+                />
             </View>
 
             <View style={ styles.contenedorTexto }>
